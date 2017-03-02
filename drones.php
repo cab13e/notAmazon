a7eb580c1b3825b982f66ec15a4ea97aaeeef143
cab13e <cab13e@acu.edu>
2017-03-01 22:07:03 -0600

    adding stuff to products page

---
+++ drones.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+$link = new mysqli("localhost","root", "password", "quadcopters");
+if ($link->connect_errno) 
+{
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
+?>
+
+<html>
+<head></head>
+<body>
+
+    <?php
+    $results = $link->query('SELECT * from table_1');
+
+    $products_list =  '<ul class="products-wrp">';
+
+while($row = $results->fetch_assoc()) {
+$products_list .= <<<EOT
+<li>
+<form class="form-item">
+<h3>{$row["model"]}</h3>
+<h4>{$row["vendor"]}</h4>
+<div><img src="{$row["image"]}"></div>
+<div>Price : {$row["msrp"]}<div>
+<div class="item-box">
+	<div>
+    Qty :
+    <select name="product_qty">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    </select>
+	</div>
+	
+    <input name="product_code" type="hidden" value="{$row["sku"]}">
+    <button type="submit">Add to Cart</button>
+</div>
+</form>
+</li>
+EOT;
+
+}
+$products_list .= '</ul></div>';
+echo $products_list;
+?>
+</body>
+</html>
